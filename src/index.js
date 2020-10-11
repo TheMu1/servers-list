8e31382902c3579d8434d19a587063dbd1726730
Eugenijus <eugenijus@profitoit.com>
2020-10-11 21:00:00 +0300

    compile css to same dir as scss to fix heroku deployment problem

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import "rc-table/assets/index.css";
-import "./Styles/css/styles.css";
+import "./Styles/styles.css";
 
 ReactDOM.render(<App/>, document.getElementById('root'));
